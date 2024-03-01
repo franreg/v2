1e82be3046c3c34b6460684b0e101f2fdb003dd9
claudeboris <konanclaudeboriskoffi@gmail.com>
2024-03-01 23:12:41 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from './pages/auth/login/Login';
+import Dashboard from './pages/admin/dashboard/Dashboard';
+import Master from './layouts/admin/Master/Master';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+          <Route path="/admin.connexion/collection-electorale" element={<Login />} />
+          <Route path='/admin.dashboard/collection-electorale' element={<Master />}>
+            <Route path='' element={<Dashboard />} />
+          </Route> 
+      </Routes>
+    </BrowserRouter>
   );
 }
 
